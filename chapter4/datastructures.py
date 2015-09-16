3e0e28d62c21fe83961a4753829989902d57ce03
eyaremenko <eugene.yaremenko@sdvi.com>
2015-09-16 16:05:36 -0700

    4.9

--- chapter4/datastructures.py
+++ chapter4/datastructures.py
@@ -14,6 +14,7 @@ class TreeNode:
         self.value = value
         self.left = left
         self.right = right
+
     def __str__(self):
         return 'TreeNode({})'.format(self.value)
 
@@ -30,6 +31,7 @@ def printTree(node: TreeNode, prefix=''):
     print(prefix, node.value)
     printTree(node.left, prefix + '    ')
     printTree(node.right, prefix + '    ')
+
 
 def buildMinBSTFromSortedList(l: list):
     def buildMinTree(l: list, start, end):
@@ -48,6 +50,7 @@ def buildMinBSTFromSortedList(l: list):
         return None
     return buildMinTree(l, 0, len(l))
 
+
 def findNode(node: TreeNode, value):
     if node is None:
         return None
@@ -57,3 +60,46 @@ def findNode(node: TreeNode, value):
     if rv is None:
         rv = findNode(node.right, value)
     return rv
+
+def searchInBST(node, elem):
+    if node is None:
+        return None
+    if node.value == elem:
+        return node
+    elif node.value > elem:
+        return searchInBST(node.left, elem)
+    else:
+        return searchInBST(node.right, elem)
+
+
+def buildBST(l: list):
+    def insertNode(node, elem):
+        if node is None:
+            return TreeNode(elem)
+        if node.value == elem:
+            return node
+        elif node.value > elem:
+            node.left = insertNode(node.left, elem)
+        else:
+            node.right = insertNode(node.right, elem)
+        return node
+
+    if l is None:
+        return None
+    node = None
+    for i in l:
+        # n = search(node, i)
+        # if n is None:
+        node = insertNode(node, i)
+    return node
+
+def areTreesEqual(node1: TreeNode, node2: TreeNode):
+    if node1 is None:
+        return node2 is None
+    if node2 is None:
+        return node1 is None
+    if node1 == node2:
+        return True
+    if node1.value == node2.value:
+        return areTreesEqual(node1.left, node2.left) and areTreesEqual(node1.right, node2.right)
+    return False
--- chapter4/p4_9.py
+++ chapter4/p4_9.py
@@ -10,3 +10,58 @@ Input:
 Output:
        {2, 1, 3}, {2, 3, 1}
 """
+
+from chapter4.datastructures import *
+
+def multiplyArrays(arr1, arr2):
+    result = []
+    if arr1 == []:
+        return [arr2]
+    if arr2 == []:
+        return [arr1]
+    head1 = arr1[0]
+    tail1 = arr1[1:]
+    for i in range(0, len(arr2) + 1):
+        prefix = arr2[0:i] + [head1]
+        allSuffixes = multiplyArrays(tail1, arr2[i:])
+        for suffix in allSuffixes:
+            result.append(prefix + suffix)
+    return result
+
+def multiplyArraySets(leftArrays, rightArrays):
+    result = []
+    for arr1 in leftArrays:
+        for arr2 in rightArrays:
+            result.extend(multiplyArrays(arr1, arr2))
+    return result
+
+def buildAllPossibleArrays(node):
+    if node is None:
+        return [[]]
+    leftArrays = buildAllPossibleArrays(node.left)
+    rightArrays = buildAllPossibleArrays(node.right)
+    arrays = []
+    suffixes = multiplyArraySets(leftArrays, rightArrays)
+    for suffix in suffixes:
+        arr = [node.value]
+        arr.extend(suffix)
+        arrays.append(arr)
+    return arrays
+
+if __name__ == '__main__':
+    # print(multiplyArrays([1, 2], [3, 4]))
+    # print(multiplyArraySets([[1, 2]], [[3, 4]]))
+    # print(multiplyArraySets([], []))
+    # print(multiplyArraySets([[], []], [[]]))
+    for k in range(2, 11):
+        l = [i for i in range(1, k)]
+        tree = buildMinBSTFromSortedList(l)
+        #printTree(tree)
+        arrs = buildAllPossibleArrays(tree)
+
+        for arr in arrs:
+            #print(arr)
+            t = buildBST(arr)
+            #printTree(t)
+            print(areTreesEqual(tree, t))
+        print(len(l), ' => ', len(arrs))
