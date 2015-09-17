c3d828caf6c34d6d7964f9cb1064de44fa1dbc77
Eugene Yaremenko <eugeneyr@gmail.com>
2015-09-17 14:03:37 -0700

    4.12

---
+++ chapter4/p4_12.py
@@ -0,0 +1,47 @@
+"""
+Paths with Sum:
+
+You are given a binary tree in which each node contains an integer value (which might be positive or negative)
+Design the algorithm to count the number of paths that sum to a given value. The path does not bneed to start or end at
+the root of a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+"""
+
+from chapter4.datastructures import *
+
+def countPathsWithSum(node: TreeNode, theSum: int):
+    def countPaths(node, theSum, path: list):
+        if node is None:
+            return 0
+        path.insert(0, node.value)
+        retval = 0
+        currSum = 0
+        for i, val in enumerate(path):
+            currSum += val
+            if currSum == theSum:
+                retval += 1
+                print(path[:i + 1])
+
+        retval += countPaths(node.left, theSum, path)
+        retval += countPaths(node.right, theSum, path)
+        path.pop(0)
+        return retval
+
+    return countPaths(node, theSum, [])
+
+import unittest
+
+class TestPathCounter(unittest.TestCase):
+    def test_countPaths(self):
+        l = [i for i in range(0, 15)]
+        tree = buildMinBSTFromSortedList(l)
+        printTree(tree)
+        ct = countPathsWithSum(tree, 13)
+        print(ct)
+        self.assertEqual(ct, 2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- chapter4/p4_12a.py
+++
@@ -1,7 +0,0 @@
-"""
-All Paths that sum up to the value
-"""
-
-from chapter4.datastructures import *
-
-def countSumPaths(node: TreeNode, theSum: int):
