75192c838a53e46e1b48075d533ecf82c5f1e05c
Eugene Yaremenko <eugeneyr@gmail.com>
2015-09-20 21:14:15 -0700

    Merge branch 'master' of ssh://diskstation/~/cracking
    # Conflicts:
    #	.idea/dataSources.xml

--- src/java/main/info/lynxnet/cracking/chapter5/Conversion56.java
+++ src/java/main/info/lynxnet/cracking/chapter5/Conversion56.java
@@ -1,7 +1,26 @@
 package info.lynxnet.cracking.chapter5;
 
-/**
- * Created by eugene on 9/20/15.
- */
+/*
+Conversion: Write a function to determine the number of bits you would need to flip to convert integer A to integer B.
+*/
 public class Conversion56 {
+    static int countOnes(int value) {
+        int count = 0;
+        while (value != 0) {
+            value = value & (value - 1);
+            count++;
+        }
+        return count;
+    }
+
+    public static int findBitDifference(int a, int b) {
+        int xorred =  a ^ b;
+        return countOnes(xorred);
+    }
+
+    public static void main(String[] args) {
+        int a = 0b11101;
+        int b = 0b01111;
+        System.out.println(findBitDifference(a, b));
+    }
 }
--- src/java/main/info/lynxnet/cracking/chapter5/PairwiseSwap57.java
+++ src/java/main/info/lynxnet/cracking/chapter5/PairwiseSwap57.java
@@ -1,7 +1,42 @@
 package info.lynxnet.cracking.chapter5;
 
 /**
- * Created by eugene on 9/20/15.
+ * Write a program to swap odd and even bits in an integer with as few instructions as possible
+ * (e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, and so on)
  */
 public class PairwiseSwap57 {
+    private static final int ODDS = 0b01010101010101010101010101010101;
+    private static final int EVENS = 0b10101010101010101010101010101010;
+
+    static String lpad(String s, char c, int length) {
+        if (s == null) {
+            return null;
+        }
+        StringBuilder builder = new StringBuilder("");
+        int lengthIWant = length - s.length();
+        while (builder.length() < lengthIWant) {
+            builder.append(c);
+        }
+        builder.append(s);
+        return builder.toString();
+    }
+
+
+    public static int swapBitPairs(int number) {
+        return (((number >>> 1) & ODDS) | (number << 1) & EVENS);
+
+        // 00101101 => (00010110 & 0101010101) | (01011010 & 1010101010) =
+        //              00010100               |  00001010
+    }
+
+    public static void main(String[] argv) {
+        System.out.println(lpad(Integer.toBinaryString(0b101101), '0', 32));
+        System.out.println(lpad(Integer.toBinaryString(swapBitPairs(0b101101)), '0', 32));
+
+        System.out.println(lpad(Integer.toBinaryString(ODDS), '0', 32));
+        System.out.println(lpad(Integer.toBinaryString(swapBitPairs(ODDS)), '0', 32));
+
+        System.out.println(lpad(Integer.toBinaryString(EVENS), '0', 32));
+        System.out.println(lpad(Integer.toBinaryString(swapBitPairs(EVENS)), '0', 32));
+    }
 }
