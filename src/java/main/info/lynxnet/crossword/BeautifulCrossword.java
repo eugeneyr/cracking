b127aee4f065b17b91ddd6502a34186beb9ba099
Eugene Yaremenko <eyaremenko@housecanary.com>
2016-09-02 08:45:06 -0700

    Added the skeleton for the BeautifulCrossword problem.

---
+++ src/java/main/info/lynxnet/crossword/BeautifulCrossword.java
@@ -0,0 +1,9 @@
+package info.lynxnet.crossword;
+
+public class BeautifulCrossword {
+    public String[] generateCrossword(int N, String[] words, int[] weights) {
+
+        return null;
+    }
+
+}
