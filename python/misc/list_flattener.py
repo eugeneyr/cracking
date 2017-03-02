e3d7864a5cbbd7b11d6f39e819165c343ae78237
eugeneyr <eugeneyr@gmail.com>
2017-03-02 14:55:01 -0800

    A stack-based list flattener.

--- python/misc/list_flattener.py
+++ python/misc/list_flattener.py
@@ -9,6 +9,22 @@ def list_flattener(a):
             rv.append(elem)
     return rv
 
+def nonrec_list_flattener(a):
+    if not isinstance(a, list):
+        return a
+    stack = a[:]
+    rv = []
+    while len(stack):
+        item = stack.pop()
+        if isinstance(item, list):
+            for x in item:
+                stack.append(x)
+        else:
+            rv.append(item)
+    rv.reverse()
+    return rv
+
 
 if __name__ == '__main__':
     print(list_flattener([['a', 'b'], 'c', ['d', [[], [['e'], 'f'], 'g']]]))
+    print(nonrec_list_flattener([['a', 'b'], 'c', ['d', [[], [['e'], 'f'], 'g']]]))
