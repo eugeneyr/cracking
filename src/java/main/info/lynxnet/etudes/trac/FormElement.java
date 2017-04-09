555a4767bb634b912499236d1cb5ba0f9ddd8c71
eugeneyr <eugeneyr@gmail.com>
2017-04-08 19:20:47 -0700

    The TRAC interpreter graduated to its own GitHub repo.

--- src/java/main/info/lynxnet/etudes/trac/FormElement.java
+++
@@ -1,9 +0,0 @@
-package info.lynxnet.etudes.trac;
-
-import java.io.Serializable;
-
-public interface FormElement extends Serializable {
-    int getOffset();
-
-    void setOffset(int offset);
-}
--- src/java/main/info/lynxnet/etudes/trac/states/InterpreterState.java
+++
@@ -1,9 +0,0 @@
-package info.lynxnet.etudes.trac.states;
-
-import info.lynxnet.etudes.trac.Context;
-
-public interface InterpreterState {
-    boolean precondition(Context context);
-    boolean isInitial();
-    Class<? extends InterpreterState> actionAndTransition(Context context);
-}
